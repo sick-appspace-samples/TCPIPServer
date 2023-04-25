d4d0ff37ecb91058f27c09b4c8a0cba6208b46af
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:02:00 +0000

    Update

--- TCPIPServer/scripts/TCPIPServer.lua
+++ TCPIPServer/scripts/TCPIPServer.lua
@@ -2,44 +2,44 @@
 --Start of Global Scope---------------------------------------------------------
 
 -- Create TCP/IP server instance
--- luacheck: globals gServer
-gServer = TCPIPServer.create()
-if not gServer then
+Server = TCPIPServer.create()
+if not Server then
   print('Could not create TCPIPServer')
 end
-TCPIPServer.setPort(gServer, 2120)
-TCPIPServer.setFraming(gServer, '\02', '\03', '\02', '\03') -- STX/ETX framing for transmit and receive
-TCPIPServer.register(gServer, 'OnConnectionAccepted', 'gHandleConnectionAccepted')
-TCPIPServer.register(gServer, 'OnConnectionClosed', 'gHandleConnectionClosed')
-TCPIPServer.register(gServer, 'OnReceive', 'gHandleReceive')
-TCPIPServer.listen(gServer)
+TCPIPServer.setPort(Server, 2120)
+TCPIPServer.setFraming(Server, '\02', '\03', '\02', '\03') -- STX/ETX framing for transmit and receive
+TCPIPServer.listen(Server)
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
 
--- Function is called when a new connection request is coming in
--- luacheck: globals gHandleConnectionAccepted
-function gHandleConnectionAccepted(con)
+--- Function is called when a new connection request is coming in
+---@param con TCPIPServer.Connection
+local function handleConnectionAccepted(con)
   print('A connection is opened: ' .. con)
   -- Send hello to the connection. If want to, the connection could be stored for later usage.
   TCPIPServer.Connection.transmit(con, 'Hello')
 end
+TCPIPServer.register(Server, 'OnConnectionAccepted', handleConnectionAccepted) -- fixed: register below the function
 
--- Function is called when a connection is closed
--- luacheck: globals gHandleConnectionClosed
-function gHandleConnectionClosed(con)
+--- Function is called when a connection is closed
+---@param con TCPIPServer.Connection
+local function handleConnectionClosed(con)
   print('A connection is closed: ' .. con)
 end
+TCPIPServer.register(Server, 'OnConnectionClosed', handleConnectionClosed)
 
--- Function is called when data is received
--- luacheck: globals gHandleReceive
-function gHandleReceive(con, data)
+--- Function is called when data is received
+---@param con TCPIPServer.Connection
+---@param data binary
+local function handleReceive(con, data)
   print('Received ' .. tostring(#data) .. ' bytes on con ' .. con)
   TCPIPServer.Connection.transmit(
     con,
     'You have sent ' .. tostring(#data) .. ' bytes'
   )
 end
+TCPIPServer.register(Server, 'OnReceive', handleReceive)
 
 --End of Function and Event Scope------------------------------------------------
--- readme.md
+++ readme.md
@@ -1,16 +1,20 @@
 ## TCPIPServer
+
 Creating and using a TCP/IP server
 
 ### Description
-The Script creates a TCPIPServer and waits for incoming connections on port 2120. 
+
+The Script creates a TCPIPServer and waits for incoming connections on port 2120.
 New connections are accepted and a greeting message is sent to them.
 Data send to the server must be wraped in STX/ETX framings in order to be processed correctly.
 The length of every received data payload is being sent back to the according client.
 
 ### How to Run
+
 For using the sample a TCP client must be installed.
 The client must be configured to connect to the localhost IP address "127.0.0.1" on port 2120 .
 The server directly sends a greeting message framed with STX/ETX.
 
 ### Topics
-System, Communication, Sample, SICK-AppSpace
+
+system, communication, sample, sick-appspace
